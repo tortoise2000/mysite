96a378e17e1b3372a824b31de824cb05f6c5ba7c
jeong <tortoise20@gmail.com>
2020-08-06 16:38:53 +0900

    first commit

---
+++ pybo/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Question
+
+def index(request):
+    """
+    pybo 목록 출력
+    """
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list': question_list}
+    return render(request, 'pybo/question_list.html', context)
